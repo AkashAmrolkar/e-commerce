3f2e873724958c0b4185ae6ee2da2d64cebb927f
AkashAmrolkar <81738189+AkashAmrolkar@users.noreply.github.com>
2023-08-22 19:40:04 +0530

    Update Categories.jsx

--- src/component/categories/Categories.jsx
+++ src/component/categories/Categories.jsx
@@ -4,7 +4,7 @@ const Categories = ({prdData}) => {
   return (
     
     <Link to={`/products/category/${prdData}`} className="w-full block h-full">
-      <div className="bg-white shadow-md border-white rounded-lg text-center py-8 text-2xl font-medium">
+      <div className="bg-white shadow-md border-white rounded-lg text-center py-8 text-2xl font-medium hover:border-orange-600 hover:text-orange-600">
         {prdData}
       </div>
     </Link>
